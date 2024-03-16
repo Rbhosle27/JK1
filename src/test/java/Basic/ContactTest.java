6956aa68bf35c4e3587cf312d4240ba6bff88680
Rushikesh <bhoslerushikesh27@gmail.com>
2024-03-16 13:16:09 +0530

    Update

---
+++ src/test/java/Basic/ContactTest.java
@@ -0,0 +1,18 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest()
+	{
+		
+		WebDriver d= new ChromeDriver();
+		d.get("http://localhost:8888/");
+
+	}
+
+}
